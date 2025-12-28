a6056be1729606a4cabc9b1e3848f44b26af4a16
Eraly1012 <eraly2320@gmail.com>
2025-12-28 10:40:07 +0500

    Add files via upload

---
+++ colorama-test.py
@@ -0,0 +1,10 @@
+import colorama
+from colorama import Fore, Back, Style
+
+print("Модуль colorama:", colorama)
+print("\nАтрибуты colorama:")
+print(dir(colorama))
+
+print("\nFore:", dir(Fore))
+print("\nBack:", dir(Back))
+print("\nStyle:", dir(Style))
